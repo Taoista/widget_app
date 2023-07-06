2e3cc4f885e474ccc4fa1fb2fbf4adb23af63994
Taoista <taoista-@hotmail.com>
2023-07-06 04:09:37 -0400

    configuracion basica de routes

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/theme/menu/menu_items.dart';
+import 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -10,6 +12,47 @@ class HomeScreen extends StatelessWidget {
         title: const Text("widghet en flutter + materiasl 3"),
         centerTitle: true,
       ),
+      body: const _HomeView(),
+    );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+  const _HomeView();
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        physics: const BouncingScrollPhysics(),
+        itemCount: appMenuItem.length,
+        itemBuilder: (context, index) {
+          final menuItem = appMenuItem[index];
+          return _CustomListTile(menuItem: menuItem);
+        });
+  }
+}
+
+class _CustomListTile extends StatelessWidget {
+  const _CustomListTile({
+    required this.menuItem,
+  });
+
+  final MenuItem menuItem;
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return ListTile(
+      leading: Icon(menuItem.icon, color: colors.primary),
+      title: Text(menuItem.title),
+      subtitle: Text(menuItem.subTitle),
+      onTap: () => {
+        // Navigator.of(context).push(
+        //   MaterialPageRoute(builder: (context) => const ButtonScreen()),
+        // ),
+        Navigator.pushNamed(context, menuItem.link)
+      },
     );
   }
 }
