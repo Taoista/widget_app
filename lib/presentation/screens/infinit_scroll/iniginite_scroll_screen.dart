e318326f1c13007dc272477ea91d249a19857249
Taoista <taoista-@hotmail.com>
2023-07-08 17:40:01 -0400

    scren creados y barril actualizado

---
+++ lib/presentation/screens/infinit_scroll/iniginite_scroll_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class InfiniteScrellScreen extends StatelessWidget {
+  static const String name = "infinite_screen";
+  const InfiniteScrellScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Placeholder();
+  }
+}
---
+++ lib/presentation/screens/progrees/progrees_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ProgressScreeen extends StatelessWidget {
+  static const String name = "progress_screen";
+  const ProgressScreeen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Placeholder();
+  }
+}
